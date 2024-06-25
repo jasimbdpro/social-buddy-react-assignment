0fa084d939c39515ea96ef7fc099477291167d9e
Jasim Uddin <jasimbdpro@gmail.com>
2024-06-25 17:37:37 +0600

    new

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { HashRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import PostDetail from './components/PostDetail/PostDetail';
+import HomeIcon from '@mui/icons-material/Home';
 
 function App() {
   return (
     <HashRouter>
-      <Link to='/'>home</Link>
+      <Link to='/'><HomeIcon></HomeIcon></Link>
       <Routes>
         <Route exact path='/' element={<Home></Home>}></Route>
         <Route path='/post/:newId' element={<PostDetail></PostDetail>} ></Route>
--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Comment from '../Comment/Comment';
 import './PostDetail.css'
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 
 const PostDetail = () => {
     const { newId } = useParams();
     const [post, setPost] = useState([]);
     const [comment, setComment] = useState([])
     const { title, body } = post;
+
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/posts/${newId}`)
             .then(res => res.json())
@@ -25,12 +27,20 @@ const PostDetail = () => {
         // if (!str) return '';
         return str && str.charAt(0).toUpperCase() + str.slice(1);
     };
+    let [likeColor, setLikeColor] = useState('primary')
+
+    const handleClick = () => {
+        setLikeColor('warning')
+        setTimeout(() => { setLikeColor('primary') }
+            , 100)
+    }
 
     return (
         <div>
             <h3>{capitalizeFirstLetter(title)}</h3>
             <p>{capitalizeFirstLetter(body) + '. ' + capitalizeFirstLetter(body) + '. ' + capitalizeFirstLetter(body) + '. ' + capitalizeFirstLetter(body)}</p>
             <h2><b>Comments...</b></h2>
+            <ThumbUpIcon onClick={handleClick} color={likeColor}></ThumbUpIcon>
             {
                 comment.map(i => <Comment comment={i} key={i.id}></Comment>)
             }
