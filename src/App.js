bb64c1b6207402e774b8ebbf7e11334a2fed21dd
Jasim Uddin <jasimbdpro@gmail.com>
2024-06-25 16:24:38 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { HashRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import PostDetail from './components/PostDetail/PostDetail';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Link to='/'>home</Link>
       <Routes>
         <Route exact path='/' element={<Home></Home>}></Route>
         <Route path='/post/:newId' element={<PostDetail></PostDetail>} ></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
